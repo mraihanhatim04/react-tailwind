86f9fbd142c03ff93699fa8aa9bd6c17e98ca2de
mraihanhatim <raihan04042001@gmail.com>
2024-10-09 18:51:26 +0700

    edit pages login, register

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import FormLogin from "../components/Fragments/FormLogin";
 import AuthLayouts from "../components/Layouts/AuthLayouts";
 
@@ -6,6 +8,12 @@ const LoginPage = () => {
     <>
       <AuthLayouts title="Login">
         <FormLogin />
+        <p className="mt-5 text-md text-center">
+          Don't have an account?{" "}
+          <Link to="/register" className="font-bold text-blue-500 underline">
+            Register
+          </Link>
+        </p>
       </AuthLayouts>
     </>
   );
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import FormRegister from "../components/Fragments/FormRegister";
 import AuthLayouts from "../components/Layouts/AuthLayouts";
 
@@ -6,6 +8,12 @@ const RegisterPage = () => {
     <>
       <AuthLayouts title="Registration">
         <FormRegister />
+        <p className="mt-5 text-md text-center">
+          Have an account?{" "}
+          <Link to="/login" className="font-bold text-blue-500 underline">
+            Login
+          </Link>
+        </p>
       </AuthLayouts>
     </>
   );
