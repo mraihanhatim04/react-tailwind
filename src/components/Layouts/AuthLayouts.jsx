1832d689c51975e7d424951e0daea691afd480ec
mraihanhatim <raihan04042001@gmail.com>
2024-10-09 18:26:13 +0700

    i react-router-dom & create router login, register

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -2,12 +2,15 @@ const AuthLayouts = (props) => {
   const { children, title } = props;
   return (
     <>
-      <div className="w-full max-w-xs">
-        <h1 className="text-4xl font-bold text-blue-500 mb-3">{title}</h1>
-        <p className="font-medium text-slate-500 mb-5">
-          Welcome, please enter your details✨
-        </p>
-        {children}
+      {" "}
+      <div className="flex min-h-screen justify-center items-center p-4">
+        <div className="w-full max-w-xs">
+          <h1 className="text-4xl font-bold text-blue-500 mb-3">{title}</h1>
+          <p className="font-medium text-slate-500 mb-5">
+            Welcome, please enter your details✨
+          </p>
+          {children}
+        </div>
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,31 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
-import App from "./App.jsx";
 import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hallo World</div>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <NextUIProvider>
-      <App />
+      <RouterProvider router={router} />
     </NextUIProvider>
   </StrictMode>
 );
